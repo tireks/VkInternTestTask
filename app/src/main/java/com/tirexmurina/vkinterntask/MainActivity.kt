4d5cd4c91d43bfc53af3e5576a1e697133732743
tireks <33616425+tireks@users.noreply.github.com>
2024-03-09 20:11:53 +0700

    navigation

--- app/src/main/java/com/tirexmurina/vkinterntask/MainActivity.kt
+++ app/src/main/java/com/tirexmurina/vkinterntask/MainActivity.kt
@@ -2,12 +2,19 @@ package com.tirexmurina.vkinterntask
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
 import dagger.hilt.EntryPoint
+import dagger.hilt.android.AndroidEntryPoint
 
-@EntryPoint
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val navController get() = findNavController(R.id.mainDataContainer)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
 }
