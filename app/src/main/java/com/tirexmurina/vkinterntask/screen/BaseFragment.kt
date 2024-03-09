f5b83707b7c2ba16dd6049f0bc69c14f56741d57
tireks <33616425+tireks@users.noreply.github.com>
2024-03-09 15:48:47 +0700

    base fragment
    made it to reduce boilerplate code of viewbinding in fragments

---
+++ app/src/main/java/com/tirexmurina/vkinterntask/screen/BaseFragment.kt
@@ -0,0 +1,24 @@
+package com.tirexmurina.vkinterntask.screen
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseFragment <VB : ViewBinding> : Fragment() {
+    private var _binding: VB? = null
+    val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        _binding = inflateViewBinding(inflater, container)
+        return binding.root
+    }
+    abstract fun inflateViewBinding(inflater: LayoutInflater, container: ViewGroup?): VB
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
