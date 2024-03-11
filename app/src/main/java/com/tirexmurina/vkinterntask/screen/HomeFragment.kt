39f9165642ff89f9996c7b0fe2f66d48474c4bd4
tireks <33616425+tireks@users.noreply.github.com>
2024-03-11 13:44:48 +0700

    some fixes and minor additions
    commas, reduced fake loading time and added ui elements for loading and error

--- app/src/main/java/com/tirexmurina/vkinterntask/screen/HomeFragment.kt
+++ app/src/main/java/com/tirexmurina/vkinterntask/screen/HomeFragment.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
@@ -53,16 +54,20 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
 
     private fun setRecyclerViews() {
         val layoutManager = GridLayoutManager(context,2)
+        //это тоже можно было заинжектить, но я подумал что это избыточно
         val adapter = HomeMainAdapter(::handleProductClick)
         binding.mainCatalogList.adapter = adapter
         binding.mainCatalogList.layoutManager = layoutManager
+        //тут то же самое, все равно листенер прикрепляется и прописывается в фрагменте
+        //так же возможно стоило листенер создавать во вьюмодели, и передавать его во фрагмент,
+        // но тогда пришлось бы как минимум контекст передать в вьюмодель, а это не очень хорошо, кажется
         val scrollListener = RecyclerViewLoadMoreScroll(GridLayoutManager(context,2))
         scrollListener.setOnLoadMoreListener(object :
             OnLoadMoreListener {
             override fun onLoadMore() {
                 adapter.addLoadingView()
                 lifecycleScope.launch {
-                    delay(3000) //это исключительно чтобы показать, что есть загрузка (без нее слишком быстро грузит)) )
+                    delay(1500) //это исключительно чтобы показать, что есть загрузка (без нее слишком быстро грузит)) )
                     expandData()
                     scrollListener.setLoaded()
                 }
@@ -80,16 +85,30 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
     }
 
     private fun showLoading() {
-        Log.d("AAA", "loading")
+        with(binding) {
+            errorContent.isVisible = false
+            contentContainer.isVisible = false
+            progressBar.isVisible = true
+        }
     }
 
     private fun showError(errorMsg: String) {
-        Log.d("AAA", "error $errorMsg")
+        with(binding) {
+            progressBar.isVisible = false
+            contentContainer.isVisible = false
+            errorContent.isVisible = true
+            errorText.text = errorMsg
+            errorButton.setOnClickListener {
+                loadData()
+            }
+        }
     }
 
     private fun showContent(items: List<Product>, expandAvailable: Boolean) {
         Log.d("AAA", "content's here")
         with(binding){
+            progressBar.isVisible = false
+            errorContent.isVisible = false
             (mainCatalogList.adapter as? HomeMainAdapter)?.rebuildData(items + null)
             if (!expandAvailable){
                 Snackbar.make(binding.mainCatalogList, getString(R.string.home_attention_snackbar_label), Snackbar.LENGTH_LONG)
