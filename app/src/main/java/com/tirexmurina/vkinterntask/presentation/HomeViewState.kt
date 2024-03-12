415a4829fc46c48a16804f077a36a31c8d71ee64
tireks <33616425+tireks@users.noreply.github.com>
2024-03-12 22:27:15 +0700

    content state now contains fields for categories

--- app/src/main/java/com/tirexmurina/vkinterntask/presentation/HomeViewState.kt
+++ app/src/main/java/com/tirexmurina/vkinterntask/presentation/HomeViewState.kt
@@ -1,10 +1,16 @@
 package com.tirexmurina.vkinterntask.presentation
 
+import com.tirexmurina.vkinterntask.domain.entity.Category
 import com.tirexmurina.vkinterntask.domain.entity.Product
 
 sealed class HomeViewState {
     data object Initial : HomeViewState()
     data object Loading : HomeViewState()
-    data class Content (val items: List<Product>, val expandAvailable : Boolean): HomeViewState()
+    data class Content (
+        val items: List<Product>,
+        val expandAvailable : Boolean,
+        val categories : List<Category>,
+        val activeCategory: Category?
+    ): HomeViewState()
     data class Error(val errorMsg: String) : HomeViewState()
 }
